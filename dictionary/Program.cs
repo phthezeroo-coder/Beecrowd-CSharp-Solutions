7b507e60c9689f62895a33e0aa00da39ac1db4c8
phthezeroo-coder <phthezeroo@gmail.com>
2026-03-09 16:01:05 -0300

    Dictionary c#

---
+++ dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+using System.Collections.Generic;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int ddd = int.Parse(Console.ReadLine());
+         
+        Dictionary<int , string> tabeladdd = new Dictionary<int, string>(); 
+        tabeladdd.Add(61 , "Brasilia");
+        tabeladdd.Add(71 , "Salvador");
+        tabeladdd.Add(11 , "Sao Paulo");
+        tabeladdd.Add(21 , "Rio de Janeiro");
+        tabeladdd.Add(32 , "Juiz de Fora");
+        tabeladdd.Add(19 , "Campinas");
+        tabeladdd.Add(27 , "Vitoria");
+        tabeladdd.Add(31 , "Belo Horizinote");
+
+        if (tabeladdd.ContainsKey(ddd))
+        {
+            Console.WriteLine(tabeladdd[ddd]);
+        }
+        else
+        {
+            Console.WriteLine("DDD nao cadastrado");
+        }
+
+
+
+    }
+
+}
